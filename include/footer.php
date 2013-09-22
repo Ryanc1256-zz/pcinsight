136b173eaa8355510001b9c83b97f8d6e65c5342
tehkhop <lachlan__davidson@hotmail.com>
2013-09-22 19:47:10 +1200

    footer spelling fix

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,5 @@
 		<div id="footer">
-			<p> copyright &copy; <?php date_default_timezone_set('Pacific/Auckland'); echo date('Y');?></p>
+			<p>Copyright &copy; <?php date_default_timezone_set('Pacific/Auckland'); echo date('Y');?></p>
 		</div>
 	</body>
 </html> 
