186be7193f680d088df767c5b92ff9b0df531edd
Ryan Clough <ryanc1256@gmail.com>
2013-10-05 09:53:19 +1300

    New style making way for the new stuff

--- index.php
+++ index.php
@@ -1,33 +1,8 @@
-<?php require_once('include/header.php'); ?>
-		<div id="innerWrapper">
-			<div id="articles">
-				<div id="imageSlider">
-					<div id="imageWrapper">
-						<div class="image">
-							<img src="http://images.anandtech.com/doci/6963/DSC02077_678x452.JPG" />
-							<span>
-								<h2>BenQ XL2720T Gaming Monitor Reviewed</h2>
-								<p> Ryan Clough </p>
-							</span>
-						</div>
-					</div>
-					<div id="Controls">
-					
-					</div>			
-				</div>
-				<?php
-					require_once('scripts/include/articles.php');
-				?>	
-			</div>
-			<div id="news">
-				<div id="newsHeader">
-					<h2> News </h2>
-				</div>
-				<div id="newsHolder">
-					<div class="news">
-						<p> Battlefield 4 has been revealed! </p>
-					</div>
-				</div>
-			</div>			
-		</div>
-<?php require_once('include/footer.php'); ?>
+<?php
+	//this file is more like a dummy file... look in the load file
+	//require ('classes/base.php');
+	include ('control/load.php');
+
+
+
+
